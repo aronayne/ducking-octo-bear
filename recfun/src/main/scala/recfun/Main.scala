225b3c28162a964323d8a28588bbd46925c74d3f
adrian-git-hub-user <adrian.ronayne@gmail.com>
2012-09-20 20:00:30 +0100

    Updated

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,10 +14,8 @@ object Main {
   /**
    * Exercise 1
    */
-/* test def pascal(c: Int, r: Int): Int = ???*/
-
   def pascal(c : Int , r : Int) : Int = 
-    innerPascal(r , c)
+   innerPascal(r , c)
 
       
   def innerPascal(r:Int, c:Int) : Int = 
@@ -29,33 +27,39 @@ object Main {
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+      
+      var counter = 0
+      var head = ' '
+      countBrackets(chars)
+      
+      def countBrackets(chars: List[Char]): Unit = {
+        
+        if(!chars.isEmpty) {
+            head = chars.head
+    		if (head == '('){
+    			counter += 1
+    		}
+            if (head == ')'){
+    		  counter -= 1
+    		}
+    		  countBrackets(chars.tail)
+        }
+      }
+      counter == 0
+      
+    }
 
   /**
    * Exercise 3
    */
-/*  def countChange(money: Int, coins: List[Int]): Int = ???
-  */
-  
-/*  def countChange(money: Int, coins: List[Int]): Int = coins match {
-  case h :: t =>
-    if (h > money) 0 else if (coins == h) 1 else countChange(money, coins - h)  + countChange(t, coins)
-  case _ => 0
-}*/ 
-  
-/*val r = f(List(1, 2, 5, 10, 20, 50, 100, 200), 200)*/
+def countChange(n: Int, ms: List[Int]): Int = 
 
-
-def f(ms: List[Int], n: Int): Int = ms match {
-  case h :: t =>
-    if (h > n) 0 else if (n == h) 1 else f(ms, n - h) + f(t, n)
-  case _ => 0
-} 
-
-  def countChange(n: Int, ms: List[Int]): Int = ms match {
+  ms match {
   case h :: t =>
     if (h > n) 0 else if (n == h) 1 else countChange(n - h , ms) + countChange(n , t)
   case _ => 0
+
 } 
   
 
